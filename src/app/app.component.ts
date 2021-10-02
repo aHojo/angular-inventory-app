3572d39e6c68936701db6b693bb3763db2776215
ahojo <ahojodev@gmail.com>
2021-10-02 15:29:13 -0400

    ngbook angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'inventory-app-root',
+  templateUrl: "./app.component.html",
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'products-ang';
+
+  products: Product[];
+
+  constructor() {
+    this.products = [
+      new Product(
+        "NICEHAT",
+        "A Nice Black Hat",
+        "assets/images/products/black-hat.jpg",
+        ["Men", "Accessories", "Hats"],
+        29.99
+      ),
+      new Product(
+        "NICE CAP",
+        "A Nice Cap ",
+        "assets/images/products/black-hat.jpg",
+        ["Men", "Accessories", "Hats"],
+        69.99
+      ),
+      new Product(
+        "NICE BEANIE",
+        "A Nice Beanie",
+        "assets/images/products/black-hat.jpg",
+        ["Men", "Accessories", "Hats"],
+        49.99
+      ),
+    ];
+  }
+
+productWasSelected(product: Product) {
+  console.log('Product Clicked ', product);
+}
+
+
 }
